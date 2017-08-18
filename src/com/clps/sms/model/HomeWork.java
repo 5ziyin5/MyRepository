c3edd4fe0a7d16760ddf053aadb6ba09f882f493
5ziyin5 <597503935@qq.com>
2017-08-18 13:37:22 +0800

    1.0

---
+++ src/com/clps/sms/model/HomeWork.java
@@ -0,0 +1,15 @@
+package com.clps.sms.model;
+
+public class HomeWork {
+	private StringBuffer workName;
+
+	public StringBuffer getWorkName() {
+		return workName;
+	}
+
+	public void setWorkName(StringBuffer workName) {
+		this.workName = workName;
+	}
+
+	
+}
